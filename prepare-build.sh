f7a86ae9bbc0b42735cdf9dfb6478daff46f26c4
Tobias Junghans <tobydox@veyon.io>
2019-03-01 10:34:09 +0100

    CI: initial import

---
+++ prepare-build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -s $(pwd)/veyon /veyon
+ln -s $(pwd)/ldap-pro /veyon/plugins/ldap-pro
+ln -s $(pwd)/licensing /veyon/plugins/licensing
+ln -s $(pwd)/network-discovery /veyon/plugins/licensing
+
