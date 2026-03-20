bc27371f94bfe9fe04371adbb025aecb05470c01
Tobias Junghans <tobydox@veyon.io>
2026-03-20 11:46:56 +0100

    Prepare 4.10.2 release

--- translations/addons_ca_ES.ts
+++ translations/addons_ca_ES.ts
@@ -516,7 +516,7 @@
     <name>EntraIdDirectory</name>
     <message>
         <source>Unknown location</source>
-        <translation type="unfinished"/>
+        <translation>Ubicació desconeguda</translation>
     </message>
     <message>
         <source>Query timed out</source>
